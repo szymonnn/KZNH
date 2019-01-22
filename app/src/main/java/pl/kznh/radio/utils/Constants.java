3496ecdb459274c9cf45be1f875c93dc4139c78a
Szymon Nitecki <szymon.nn@gmail.com>
2019-01-22 20:09:41 +0100

    updated version

--- app/src/main/java/pl/kznh/radio/utils/Constants.java
+++ app/src/main/java/pl/kznh/radio/utils/Constants.java
@@ -85,7 +85,7 @@ public class Constants {
 
     public static final String KEY_PLAN_MY_SWORD_ABBREVIATION = "MySwordAbbreviation";
 
-    public static final String CLASS_READING_PLAN = "ReadingPlan2";
+    public static final String CLASS_READING_PLAN = "ReadingPlan";
 
     public static final String PREF_READ_FRAGMENTS = "read_positions";
 }
